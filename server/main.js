441afa4d7575e3a877e16ae47a88f38559182679
bbosch <bbosch@deloitte.nl>
2023-05-20 16:02:54 +0200

    Now using a map because it's "cool"

--- server/main.js
+++ server/main.js
@@ -7,11 +7,24 @@ const serverDomain = "127.0.0.1"
 const serverPort = 3000
 
 const STATUS_PENDING = 0
-var sessions = {}
+var sessions = new Map()
 
 function genUUID()
 {
     return crypto.randomBytes(20).toString('hex')
+}
+
+
+function cleanSessions()
+{
+    const now = Date.now()
+
+    sessions.forEach((value, key) => {
+        if (now - value.timestamp > 1000 * 60 * 60) {
+            sessions.delete(key)
+            console.log("Deleted old key ", key)
+        }
+    })
 }
 
 function resolveBody(req, body)
@@ -26,22 +39,37 @@ function resolveBody(req, body)
 
     if (req.method === "POST" && req.url.endsWith("/session-status"))
     {
-        const status = sessions[jsonbody["sessionid"]]
-
-        // If the session has no response yet, do this
         json_response = {
-            status : status === undefined ?  400 : 200,
-            sessionstatus: status,
-            sessionmessage: status === undefined ? undefined : jsonbody["proofstring"]
+            status : 400
+        }
+
+        try{
+            const status = sessions.get(jsonbody["sessionid"]).status
+            const sessiondate = sessions.get(jsonbody["sessionid"]).timestamp
+
+            // If the session has no response yet, do this
+            json_response = {
+                status : 200,
+                sessionstatus: status,
+                sessioncreated: sessiondate
+            }
+        }
+        catch (err) {
+            console.log(err)
         }
     } 
 
     else if (req.method === "POST" && req.url.endsWith("/create-session"))
     {
+        cleanSessions()
+
         console.log(sessions)
 
         const newid = genUUID() 
-        sessions[newid] = STATUS_PENDING
+        sessions.set(newid, {
+            status: STATUS_PENDING, 
+            timestamp: Date.now()
+        })
 
         json_response = {
             status : 200 , 
@@ -55,12 +83,23 @@ function resolveBody(req, body)
             status : 400
         }
 
-        if (sessions[jsonbody["sessionid"]] !== undefined) {
-            sessions[jsonbody["sessionid"]] = jsonbody["proofstring"]
+        console.log(`Trying to get ${jsonbody["sessionid"]} from 'sessions'`)
+
+        try {
+            
+            const modifiedSession = sessions.get(jsonbody["sessionid"])
+            console.log(modifiedSession)
+            modifiedSession.status = jsonbody["proofstring"]
+            console.log(modifiedSession)
+            
+            sessions.set(jsonbody["sessionid"], modifiedSession)
 
             json_response = {
                 status : 200
             }
+        }
+        catch (err) {
+            console.log(err)
         }
     }
 
