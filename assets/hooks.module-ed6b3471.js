107cb61d72bda2504d1cd95892c9c81dc618707f
Hicham010 <47334105+Hicham010@users.noreply.github.com>
2023-05-20 10:46:45 +0200

    Updates

--- assets/hooks.module-ed6b3471.js
+++ assets/hooks.module-ed6b3471.js
@@ -1,4 +1,4 @@
-import{c as he}from"./index-98fd78bd.js";var Oe={exports:{}};/*! *****************************************************************************
+import{c as he}from"./index-7bb44d7e.js";var Oe={exports:{}};/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
